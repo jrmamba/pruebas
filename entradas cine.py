726c171c9f58755e15f5cab2092859126579c5c5
jrmamba <98396017+jrmamba@users.noreply.github.com>
2022-01-25 12:56:27 +0100

    Add files via upload

---
+++ entradas cine.py
@@ -0,0 +1,12 @@
+entradas = 6
+precio_entradas = 7.25
+
+total_entradas = entradas * precio_entradas
+
+
+dinero_entregado = 50
+
+vuelta_dinero = dinero_entregado - total_entradas
+
+
+print(vuelta_dinero)
